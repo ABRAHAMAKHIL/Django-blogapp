f789bff062fd6e2e2e594a370e87ed5b0ac6e062
Akhil Reji Abraham <akhilabraham34@gmail.com>
2023-04-08 01:04:31 +0530

    Added Responsiveness

--- Blog/models.py
+++ Blog/models.py
@@ -17,3 +17,12 @@ class Post(models.Model):
          ordering = ['-Updated','-Created']
      def __str__(self):
          return self.Title
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Post,on_delete=models.CASCADE)
+    body = models.TextField()
+    updated  = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.body[0:50]
